789936009e933cf06d39fd7462c28219424cafad
unknown <antoniotx.dev@gmail.com>
2019-02-13 13:31:40 -0200

    Adiciona rota de cadastro de clientes

--- routes/clientes.js
+++ routes/clientes.js
@@ -1,8 +1,38 @@
 const erros = require('restify-errors')
+const Cliente = require('../models/Cliente')
 
 module.exports = server => {
-    server.get('/clientes', (req, res, next) => {
-        res.send({ msg: 'test' })
-        next()
+    // Lista Clientes
+    server.get('/clientes', async (req, res, next) => {
+        try {
+            const clientes = await Cliente.find({})
+            res.send(clientes)
+            next()
+        } catch(erro) {
+          return next(new erros.InvalidContentError(erro))  
+        }
+    })
+
+    // Cadastra Cliente
+    server.post('/clientes', async (req, res, next) => {
+        if(!req.is('application/json')){
+            return new(new erros.InvalidContentError("Era esperado 'application/json'"))
+        }
+
+        const { nome, email, balanco } = req.body
+
+        const cliente = new Cliente({
+            nome,
+            email,
+            balanco
+        })
+
+        try {
+            const novoCliente = await cliente.save()
+            res.send(201)
+            next()
+        }catch(erro){
+            return next(new erros.InternalError(erro.message))
+        }
     })
 }
