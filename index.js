1d7ef8fc76e396782864706d2262cbde70c5221c
unknown <antoniotx.dev@gmail.com>
2019-02-15 21:51:00 -0200

    remove proteção de rotas

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ server.use(restify.plugins.bodyParser())
 // server.use(rjwt({ secret: config.JWT_SECRET }).unless({ path: ['/auth'] }))
 
 // Serve static assets if in production
-if(process.env.NODE_ENV === 'production'){
-    // Set static folder
-    app.use(express.static('client/build'))
+// if(process.env.NODE_ENV === 'production'){
+//     // Set static folder
+//     app.use(express.static('client/build'))
 
-    app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+//     app.get('*', (req, res) =>{
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
 server.listen(config.PORT, () => {
     // Remove warning bugado
